52adc2f70dfda97ccd901b60faaea3d0619ebe37
Jam <104766756+Ihor-Slastonov@users.noreply.github.com>
2024-02-26 17:29:02 +0200

    configure axios

--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -1,2 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 
+import { PLAYPIK_BASE_URL } from '../utils/constans/urls';
+
+const playpikApi = axios.create({
+  baseURL: PLAYPIK_BASE_URL,
+});
+
+export { playpikApi };
---
+++ src/utils/constans/urls.js
@@ -0,0 +1,2 @@
+export const PLAYPIK_BASE_URL =
+  'https://65dca7abe7edadead7eca3ee.mockapi.io/v1/';
