8ab843625ee3a662775c784a8fd5591146b7f476
Jam <104766756+Ihor-Slastonov@users.noreply.github.com>
2024-02-26 14:10:23 +0200

    configure initial project
    Install axios, tailwind css, configure tailwind and data structure

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors';
+
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      black: colors.black,
+      white: colors.white,
+
+      dark: '#222831',
+      'semi-dark': '#393E46',
+      accent: '#FFD369',
+      light: '#EEEEEE',
+    },
+
+    fontFamily: {
+      myPlay: ['MyPlay', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
