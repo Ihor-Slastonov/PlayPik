56e74f97bd2de40ad1be291e373cf331d8a76e93
Jam <104766756+Ihor-Slastonov@users.noreply.github.com>
2024-04-19 15:38:46 +0300

    Update Card.jsx

--- src/components/CardList/Card.jsx
+++ src/components/CardList/Card.jsx
@@ -1,5 +1,7 @@
 import PropTypes from 'prop-types';
 
+import { TrashIcon } from '@heroicons/react/24/solid';
+
 const Card = ({ imgURL, title, category }) => {
   return (
     <li className="card__container">
@@ -7,6 +9,14 @@ const Card = ({ imgURL, title, category }) => {
       <p className="card__title">{title}</p>
 
       <span className="card__label">{category}</span>
+
+      <button
+        className="absolute top-4 left-1 w-8 h-8
+        rounded-md text-accent_red bg-dark
+      flex items-center justify-center border border-accent_red"
+      >
+        <TrashIcon className="w-6 h-6" />
+      </button>
     </li>
   );
 };
