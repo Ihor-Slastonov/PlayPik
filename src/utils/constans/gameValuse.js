77873ca2ad5e338817851555941ada79c42b54e0
Jam <104766756+Ihor-Slastonov@users.noreply.github.com>
2024-04-27 12:04:21 +0300

    Delete previos filter components.

--- src/utils/constans/gameValuse.js
+++ src/utils/constans/gameValuse.js
@@ -1,3 +1,5 @@
 export const gameTypes = ['single', 'multiplayer'];
 
 export const gameCategories = ['pc', 'pc vr', 'quest'];
+
+export const filterCategories = ['pc', 'pc vr', 'pc + pc vr', 'quest'];
