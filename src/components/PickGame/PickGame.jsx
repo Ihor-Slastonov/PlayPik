dfe92c316eca697ebf8a970312ac3533e7b1b4fd
Jam <104766756+Ihor-Slastonov@users.noreply.github.com>
2024-07-11 18:48:08 +0300

    Update PickGame.jsx

--- src/components/PickGame/PickGame.jsx
+++ src/components/PickGame/PickGame.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react';
 
-import useCountdownTimer from '../../utils/hooks/useCountdownTimer';
-
 import Modal from '../Modal/Modal';
 import { usePlayPik } from '../../utils/hooks/usePlayPik';
 import PickGameCard from './PickGameCard';
@@ -11,7 +9,7 @@ import PickGameBtn from './PickGameBtn';
 const PickGame = () => {
   const [randomGame, setRandomGame] = useState(null);
   const [isOpen, setIsOpen] = useState(false);
-  const { startTimer, resetTimer, countdown } = useCountdownTimer(3, 1000);
+
   const { games } = usePlayPik();
 
   const pickRandomGame = () => {
@@ -22,12 +20,10 @@ const PickGame = () => {
   const handleClick = () => {
     pickRandomGame();
     setIsOpen(true);
-    startTimer();
   };
 
   const handleClose = () => {
     setIsOpen(false);
-    resetTimer();
   };
 
   return (
@@ -40,11 +36,7 @@ const PickGame = () => {
         closeModal={handleClose}
         modalLayout={false}
       >
-        <PickGameCard
-          handleClose={handleClose}
-          game={randomGame}
-          countdown={countdown}
-        />
+        <PickGameCard handleClose={handleClose} game={randomGame} />
       </Modal>
     </>
   );
