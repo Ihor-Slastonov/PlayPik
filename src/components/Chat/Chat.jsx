5b7b22fd24d6428f791d2106689a1d4546526cb2
Jam <104766756+Ihor-Slastonov@users.noreply.github.com>
2024-08-04 00:36:40 +0300

    add chat button

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,21 +1,9 @@
-import { ChatBubbleLeftRightIcon } from '@heroicons/react/24/outline';
+import ChatBtn from './ChatBtn';
 
 const Chat = () => {
   return (
     <>
-      <button
-        type="button"
-        className="grid place-content-center size-10 rounded-md 
-        border border-semi-dark opacity-50 
-        duration-500 ease-in-out
-        hover:border-accent group"
-      >
-        <ChatBubbleLeftRightIcon
-          className="size-6 text-semi-dark opacity-50 
-        group-hover:opacity-100 group-hover:text-accent_green group-hover:animate-pulse
-        duration-500 ease-in-out"
-        />
-      </button>
+      <ChatBtn />
     </>
   );
 };
