c3f8365299df9fedeca70d2e3c8c60e11f82a51f
Jam <104766756+Ihor-Slastonov@users.noreply.github.com>
2024-08-05 16:04:52 +0300

    Update ChatOnlineList.jsx

--- src/components/Chat/ChatOnlineList.jsx
+++ src/components/Chat/ChatOnlineList.jsx
@@ -2,12 +2,12 @@ import PropTypes from 'prop-types';
 
 const ChatOnlineList = ({ nicknameList }) => {
   return (
-    <ul className="w-20 h-full border absolute top-0 right-0 px-1">
+    <ul className="w-20 h-full border border-accent border-l-accent_green absolute top-0 right-0 px-2">
       <p>Online</p>
       {nicknameList?.map(nick => (
         <li key={nick.id} className="flex items-center gap-2 w-full">
           <div className="block w-2 h-2 bg-accent_green rounded-full flex-shrink-0" />
-              <p className="truncate flex-grow">{nick.nick}</p>
+          <p className="truncate flex-grow">{nick.nick}</p>
         </li>
       ))}
     </ul>
