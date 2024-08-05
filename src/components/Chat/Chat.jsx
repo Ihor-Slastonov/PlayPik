2721336ed9caf3ce50a71d7b73a8f99e02a09697
Jam <104766756+Ihor-Slastonov@users.noreply.github.com>
2024-08-05 21:00:27 +0300

    add socket connection

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,47 +1,64 @@
+const CHAT_SERVER_URL = import.meta.env.VITE_CHAT_SERVER_URL;
+
 import { useEffect, useState } from 'react';
+
 import { nanoid } from 'nanoid';
+import axios from 'axios';
 
 import ChatBtn from './ChatBtn';
 import ChatContainer from './ChatContainer';
 import ChatInputName from './ChatInputName';
 import ChatOnlineList from './ChatOnlineList';
-import axios from 'axios';
+import { socket } from '../../services/socket';
+import toast from 'react-hot-toast';
 
 const Chat = () => {
   const [nickname, setNickname] = useState('');
   const [nicknameList, setNicknameList] = useState([]);
-  const [massegeList, setMassegeList] = useState([]);
+  // const [massegeList, setMassegeList] = useState([]);
   const [message, setMessage] = useState('');
   const [isLoaded, setIsLoaded] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
+  // const [newSocket, setNewSocket] = useState(null);
 
   useEffect(() => {
     const connectionToServer = async () => {
       try {
-        const res = await axios.get('http://localhost:3002');
-        // const res = await axios.get('https://playpickchat.onrender.com');
+        const res = await axios.get(CHAT_SERVER_URL);
         if (!res) return;
         setMessage(res.data.message);
       } catch (error) {
         console.log(error);
+        toast.error('Error connection');
       } finally {
         setIsLoaded(true);
+        socket.connect();
       }
     };
-
     connectionToServer();
+    socket.on('connection', events => {
+      console.log(events);
+    });
+    socket.on('userList', users => {
+      setNicknameList(users);
+    });
+
+    return () => {
+      socket.disconnect();
+      socket.off('connection');
+    };
   }, []);
 
   const handleToogle = () => setIsOpen(prev => !prev);
+
   const handleSetNameAndAddToList = name => {
     setNickname(name);
-    setNicknameList(prev => [
-      ...prev,
-      {
-        nick: name,
-        id: nanoid(),
-      },
-    ]);
+    const newUser = {
+      id: nanoid(),
+      name: name,
+    };
+
+    socket.emit('newUser', newUser);
   };
 
   return (
@@ -57,14 +74,13 @@ const Chat = () => {
             {!nickname && (
               <ChatInputName setNickname={handleSetNameAndAddToList} />
             )}
-            <div className="w-full h-full flex border-accent_red">
-              <div className="w-full">
-                {nickname && (
-                  <h2 className="text-2xl text-center mb-2">{message}</h2>
-                )}
-              </div>
-              {nickname && <ChatOnlineList nicknameList={nicknameList} />}
+
+            <div className="w-full">
+              {nickname && (
+                <h2 className="text-2xl text-center mb-2">{message}</h2>
+              )}
             </div>
+            {nickname && <ChatOnlineList nicknameList={nicknameList} />}
           </>
         )}
       </ChatContainer>
