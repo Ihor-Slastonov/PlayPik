026091f41c729e0ec59dd2c1165ed5ce7d6e1ea4
Jam <104766756+Ihor-Slastonov@users.noreply.github.com>
2024-08-05 15:18:23 +0300

    Update ChatInputName.jsx

--- src/components/Chat/ChatInputName.jsx
+++ src/components/Chat/ChatInputName.jsx
@@ -16,16 +16,27 @@ const ChatInputName = ({ setNickname }) => {
   };
 
   return (
-    <form onSubmit={onFormSubmit}>
-      <label>
+    <form
+      onSubmit={onFormSubmit}
+      className="min-w-[200px] p-2 absolute top-8 left-1/2 -translate-x-1/2
+      border border-accent_green rounded-lg"
+    >
+      <label className="mb-1">
         Enter Your Nickname
         <input
           type="text"
-          className="text-dark"
+          className="text-dark w-full px-1 text-2xl"
           onChange={handleOnChangeInput}
           value={inputValue}
         />
       </label>
+      <div
+        className="w-full h-6 absolute -bottom-[30px] left-0 
+       bg-accent_green rounded-md blur-sm"
+      ></div>
+      <p className="absolute rotate-180 -scale-x-100 text-2xl blur-[1px] top-[74px] left-3 text-accent ">
+        {inputValue}
+      </p>
     </form>
   );
 };
