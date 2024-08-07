3d051b3608962c297da7832ba2999d79617d4766
Jam <104766756+Ihor-Slastonov@users.noreply.github.com>
2024-08-07 21:18:32 +0300

    add nav panel

---
+++ src/components/NavPanel/NavPanel.jsx
@@ -0,0 +1,31 @@
+import { Link, useLocation } from 'react-router-dom';
+
+const NavPanel = () => {
+  const location = useLocation();
+
+  const linkText = location.pathname !== '/' ? 'Home' : 'Tournament';
+
+  return (
+    <div className="w-full absolute top-0 left-0 flex items-center justify-between">
+      <nav
+        className="flex items-center gap-2
+            "
+      >
+        mode:
+        <Link
+          to={location.pathname === '/' ? '/tournament' : '/'}
+          className="p-2 rounded-lg border border-accent 
+                hover:text-accent hover:border-accent_green duration-500 ease-in-out"
+        >
+          {linkText}
+        </Link>
+      </nav>
+
+      <p className="flex items-center gap-2">
+        last game: <span>Outlast</span>
+      </p>
+    </div>
+  );
+};
+
+export default NavPanel;
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,34 +1,21 @@
 import { Suspense } from 'react';
-import { Link, Outlet, useLocation } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 
 import FilterBar from '../FilterBar/FilterBar';
 import PickGame from '../PickGame/PickGame';
 import Chat from '../Chat/Chat';
 import ReloadDataBtn from '../ReloadDataBtn/ReloadDataBtn';
+import NavPanel from '../NavPanel/NavPanel';
 
 const SharedLayout = () => {
   const location = useLocation();
 
-  const linkText = location.pathname !== '/' ? 'Home' : 'Tournament';
   return (
     <>
       <header className="py-5 sticky top-0 z-10 bg-dark border-b shadow-md shadow-semi-dark">
         <div className="container">
           <div className="flex flex-col gap-4 relative">
-            <p
-              className="absolute top-0 left-0 flex items-center gap-2
-            "
-            >
-              mode:
-              <Link
-                to={location.pathname === '/' ? '/tournament' : '/'}
-                className="p-2 rounded-lg border border-accent 
-                hover:text-accent hover:border-accent_green duration-500 ease-in-out"
-              >
-                {linkText}
-              </Link>
-            </p>
-
+            <NavPanel />
             <PickGame />
             {location.pathname === '/' && <FilterBar />}
           </div>
