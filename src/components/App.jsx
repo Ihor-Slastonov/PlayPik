1a85c8c906d1598b91c36e6bd0ae655ca2413ceb
Jam <104766756+Ihor-Slastonov@users.noreply.github.com>
2024-08-18 14:52:30 +0300

    add pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,29 @@
 import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
+
+import AuthLayout from './AuthLayout';
 import SharedLayout from './SharedLayout/SharedLayout';
+
 import NotFound from '../pages/NotFound';
 
+const LoginPage = lazy(() => import('../pages/LoginPage'));
+const RegisterPage = lazy(() => import('../pages/RegisterPage'));
 const Home = lazy(() => import('../pages/Home'));
 const TournamentPage = lazy(() => import('../pages/TournamentPage'));
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        <Route index element={<Home />} />
-        <Route path="/tournament" element={<TournamentPage />} />
-        <Route path="*" element={<NotFound />} />
+      <Route path="/" element={AuthLayout}>
+        <Route index element={<LoginPage />} />
+        <Route path="sign-up" element={<RegisterPage />} />
       </Route>
+
+      {/* <Route path="/mode" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/mode/tournament" element={<TournamentPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Route> */}
     </Routes>
   );
 };
