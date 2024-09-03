7df3f091843c71b358b71321e9ba93c0bc1003dd
Jam <104766756+Ihor-Slastonov@users.noreply.github.com>
2024-09-03 16:02:39 +0300

    Update AuthLayoutNav.jsx

--- src/components/AuthLayout/AuthLayoutNav.jsx
+++ src/components/AuthLayout/AuthLayoutNav.jsx
@@ -1,27 +1,47 @@
 import { NavLink } from 'react-router-dom';
+import { useAuth } from '../../utils/hooks/useAuth';
+import { selectUser } from '../../zustand/auth/authSelectors';
 
 const AuthLayoutNav = () => {
+  const { isLoggedIn } = useAuth();
+  const user = selectUser();
   return (
     <nav className="grid grid-cols-2 gap-x-4 gap-y-3">
-      <NavLink
-        to="/login"
-        className={({ isActive }) =>
-          isActive ? 'btn_inline_green' : 'btn_outline_green'
-        }
-      >
-        Sign In
-      </NavLink>
-      <NavLink
-        to="/register"
-        className={({ isActive }) =>
-          isActive ? 'btn_inline_yellow' : 'btn_outline_yellow'
-        }
-      >
-        Sign Up
-      </NavLink>
-      <NavLink to="/mode" className="btn_outline_red w-full col-span-2" replace>
-        Skip
-      </NavLink>
+      {!isLoggedIn ? (
+        <>
+          <NavLink
+            to="/login"
+            className={({ isActive }) =>
+              isActive ? 'btn_inline_green' : 'btn_outline_green'
+            }
+          >
+            Sign In
+          </NavLink>
+          <NavLink
+            to="/register"
+            className={({ isActive }) =>
+              isActive ? 'btn_inline_yellow' : 'btn_outline_yellow'
+            }
+          >
+            Sign Up
+          </NavLink>
+          <NavLink
+            to="/mode"
+            className="btn_outline_red w-full col-span-2"
+            replace
+          >
+            Skip
+          </NavLink>
+        </>
+      ) : (
+        <NavLink
+          to="/mode"
+          className="btn_outline_green w-full col-span-2"
+          replace
+        >
+          Welcome {user.name}
+        </NavLink>
+      )}
     </nav>
   );
 };
