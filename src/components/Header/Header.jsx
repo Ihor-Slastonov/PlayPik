ed6f5d949bf29dba31ea8b7c4f2da52fc3d5e55b
Jam <104766756+Ihor-Slastonov@users.noreply.github.com>
2024-09-05 15:19:24 +0300

    add header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="w-full h-20 border">
+      <div className="container border border-accent_red"></div>
+    </header>
+  );
+};
+
+export default Header;
--- src/components/ModeLayout/ModeLayout.jsx
+++ src/components/ModeLayout/ModeLayout.jsx
@@ -1,10 +1,11 @@
 import React, { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
+import Header from '../Header/Header';
 
 const ModeLayout = () => {
   return (
     <>
-      <div>ModeLayout</div>
+      <Header />
       <Suspense fallback={<div>Loading...</div>}>
         <Outlet />
       </Suspense>
