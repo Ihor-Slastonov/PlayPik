3d2562aafee5afdfabae9dfc65ffc7075a1314be
Jam <104766756+Ihor-Slastonov@users.noreply.github.com>
2024-09-10 17:38:47 +0300

    delete values

--- src/utils/constans/gameValuse.js
+++
@@ -1,9 +0,0 @@
-export const gameTypes = ['single', 'multiplayer'];
-
-export const gameCategories = ['pc', 'pc vr', 'quest'];
-
-export const filterCategories = ['all', 'pc', 'pc vr', 'pc + pc vr', 'quest'];
-
-export const filterTypes = ['all', 'single', 'multiplayer'];
-
-export const gameListTypes = ['all', 'favorite'];
--- src/utils/constans/initialValues.js
+++ src/utils/constans/initialValues.js
@@ -1,14 +1,3 @@
-import { gameCategories, gameTypes } from './gameValuse';
-
-export const addGameFormInitialValues = {
-  title: '',
-  imgURL: '',
-  rating: 0,
-  file: '',
-  category: gameCategories ? gameCategories[0] : '',
-  type: gameTypes ? gameTypes[1] : '',
-};
-
 export const authInitialState = {
   user: {
     id: '',
