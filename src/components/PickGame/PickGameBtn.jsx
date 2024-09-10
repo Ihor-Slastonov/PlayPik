a44fdfbc46fcd985bc8493f07fed8ae05b2217fd
Jam <104766756+Ihor-Slastonov@users.noreply.github.com>
2024-09-10 17:41:52 +0300

    update for desktop

--- src/components/PickGame/PickGameBtn.jsx
+++ src/components/PickGame/PickGameBtn.jsx
@@ -2,11 +2,11 @@ import { PropTypes } from 'prop-types';
 
 const PickGameBtn = ({ handleClick }) => {
   return (
-    <div className="relative mx-auto block h-16 w-36">
+    <div className="relative mx-auto block h-16 w-36 xl:h-24 xl:w-56">
       <button
         type="button"
-        className=" mx-auto block h-full w-full
-      rounded-md bg-[url('/button-2.webp')] bg-cover bg-center bg-no-repeat"
+        className=" mx-auto block h-full w-full rounded-md
+      bg-dark bg-[url('/button-2.webp')] bg-cover bg-center bg-no-repeat"
         onClick={handleClick}
       ></button>
       <div
