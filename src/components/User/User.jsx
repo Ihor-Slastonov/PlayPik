16a4fcc5af40593d5dfb2d865b435c712cd927d7
Jam <104766756+Ihor-Slastonov@users.noreply.github.com>
2024-09-11 15:25:43 +0300

    add buttons to user

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,17 +1,39 @@
+import { selectUser } from '../../zustand/auth/authSelectors';
+
 import UserAvatar from './UserAvatar';
+import {
+  AdminUserBtn,
+  LogoutUserBtn,
+  NotifyUserBtn,
+  SettingsUserBtn,
+} from './UserButtons';
 import UserInfo from './UserInfo';
 
 const User = () => {
+  const { role } = selectUser();
   return (
     <div
       className="beforeLine_green relative h-[140px] w-full 
     py-4 shadow-md shadow-black xl:h-[200px] xl:py-2.5"
     >
+      {/* User buttons */}
+      <LogoutUserBtn />
+      <div
+        className="absolute left-24 top-4 flex flex-row-reverse 
+     gap-4 xl:right-4 xl:flex-col-reverse xl:items-end"
+      >
+        {role !== 'user' && <AdminUserBtn />}
+        <NotifyUserBtn />
+        <SettingsUserBtn />
+      </div>
+
+      {/* User Avatar */}
       <div className="flex flex-col gap-4 px-4">
         <div className="xl:mx-auto">
           <UserAvatar />
         </div>
 
+        {/* User info */}
         <UserInfo />
       </div>
     </div>
---
+++ src/components/User/UserButtons.jsx
@@ -0,0 +1,56 @@
+import {
+  Cog6ToothIcon,
+  EnvelopeIcon,
+  ArrowLeftStartOnRectangleIcon,
+  UserIcon,
+} from '@heroicons/react/24/outline';
+
+export const LogoutUserBtn = () => {
+  return (
+    <button
+      type="button"
+      className="center absolute bottom-4 right-4 flex size-10 text-semi-dark 
+       duration-500 ease-in-out hover:text-accent_red
+       focus:text-accent_red xl:left-4 xl:top-4"
+    >
+      <ArrowLeftStartOnRectangleIcon className="size-8" />
+    </button>
+  );
+};
+
+export const NotifyUserBtn = () => {
+  return (
+    <button
+      type="button"
+      className="center flex size-14 text-semi-dark 
+      transition-colors duration-500 ease-in-out 
+      hover:text-accent_green focus:text-accent_green xl:size-10"
+    >
+      <EnvelopeIcon className="size-10" />
+    </button>
+  );
+};
+
+export const SettingsUserBtn = () => {
+  return (
+    <button
+      type="button"
+      className="center flex size-14 text-semi-dark transition-colors
+    duration-500 ease-in-out hover:text-accent focus:text-accent xl:size-10"
+    >
+      <Cog6ToothIcon className="size-10" />
+    </button>
+  );
+};
+
+export const AdminUserBtn = () => {
+  return (
+    <button
+      type="button"
+      className="center flex size-14 text-semi-dark transition-colors
+    duration-500 ease-in-out hover:text-accent focus:text-accent xl:mt-6 xl:size-10"
+    >
+      <UserIcon className="size-10" />
+    </button>
+  );
+};
