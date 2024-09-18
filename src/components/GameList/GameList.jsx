3fcfb3d8f255f472b6cd1b38e5245385786e82ba
Jam <104766756+Ihor-Slastonov@users.noreply.github.com>
2024-09-18 15:17:13 +0300

    Update GameList.jsx

--- src/components/GameList/GameList.jsx
+++ src/components/GameList/GameList.jsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from 'react';
 import GameCard from './GameCard';
 
 import {
@@ -5,11 +6,29 @@ import {
   selectTypes,
 } from '../../zustand/filters/filtersSelectors';
 import { gamesSelector } from '../../zustand/games/gamesSelectors';
+import gsap from 'gsap';
 
 const GameList = () => {
   const games = gamesSelector();
   const categories = selectCategories();
   const types = selectTypes();
+  const listRef = useRef(null);
+
+  useEffect(() => {
+    const listItems = listRef.current;
+    gsap.fromTo(
+      listItems,
+      { opacity: 0, scale: 1.1, filter: 'blur(20px)' }, // Начальное состояние
+      {
+        opacity: 1,
+
+        scale: 1,
+        duration: 0.8,
+        ease: 'power2.out',
+        filter: 'blur(0px)',
+      }
+    );
+  }, [games]);
 
   const getCategoryName = categoryId => {
     const category = categories.find(cat => cat.id === categoryId);
@@ -23,6 +42,7 @@ const GameList = () => {
 
   return (
     <ul
+      ref={listRef}
       className="flex flex-col items-center gap-4 
     xl:flex-row xl:flex-wrap xl:justify-center xl:gap-6"
     >
